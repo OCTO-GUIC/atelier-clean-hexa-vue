81f82be036b59e40da81c6341f912cdadb9ae021
dlamande <contact@dorianlamande.com>
2025-05-06 13:13:24 +0200

    ajout d'un test unitaire

---
+++ tests/domains/weapons/getAllWeapons.usecase.spec.ts
@@ -0,0 +1,50 @@
+import { describe, it, expect, vi } from 'vitest'
+import { WeaponsRepository } from '@/domains/weapons/ports/weapons.repository'
+import { GetAllWeaponsUsecase, Weapons } from '@/domains/weapons/getAllWeapons.usecase'
+import { WeaponsCataloguePresenter } from '@/domains/weapons/adapters/weapons.presenter.catalog'
+
+describe('GetAllWeaponsUsecase', () => {
+  it('should retrieve all weapons and present them with available gold amount', async () => {
+    // GIVEN
+    const mockWeapons = [
+      new Weapons('1', 'Sword', 'sword.jpg', 100),
+      new Weapons('2', 'Bow', 'bow.jpg', 150),
+    ]
+
+    const mockRepository: WeaponsRepository = {
+      getAllWeapons: vi.fn().mockResolvedValue(mockWeapons),
+    }
+
+    const usecase = new GetAllWeaponsUsecase(mockRepository)
+    const goldAmount = 100
+
+    // WHEN // TH
+    await usecase.execute(
+      goldAmount,
+      new WeaponsCataloguePresenter((vm) => {
+        expect(vm).toEqual({
+          items: [
+            {
+              addToCartButton: {
+                disabled: false,
+                label: 'Acheter',
+              },
+              image: 'public/images/sword.jpg',
+              price: 100,
+              title: 'Sword',
+            },
+            {
+              addToCartButton: {
+                disabled: true,
+                label: 'Acheter',
+              },
+              image: 'public/images/bow.jpg',
+              price: 150,
+              title: 'Bow',
+            },
+          ],
+        })
+      }),
+    )
+  })
+})
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,19 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from './vite.config'
+/// <reference types="vitest" />
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-  }),
-)
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['tests/**/*.{test,spec}.ts'],
+  },
+})
